146a4c92eb5d8315a7a51947444ea16a1567df80
Kimyen <kimyen0816@gmail.com>
2017-08-15 14:30:26 -0700

    Modify DESCRIPTION file to add dev version

--- jenkins.sh
+++ jenkins.sh
@@ -10,7 +10,17 @@ rm -rf ../RLIB
 mkdir -p ../RLIB
 
 PACKAGE_NAME=synapser
-PACKAGE_VERSION=`grep Version DESCRIPTION | awk '{print $2}'`.${BUILD_NUMBER}
+
+if [[ ${DEV_VERSION} ]] 
+then
+	VERSION_LINE=`grep Version DESCRIPTION`
+	DEV_VERSION_LINE=$VERSION_LINE.$DEV_VERSION
+	sed "s|$VERSION_LINE|$DEV_VERSION_LINE|g" DESCRIPTION > DESCRIPTION.temp
+	rm DESCRIPTION
+	mv DESCRIPTION.temp DESCRIPTION
+fi
+
+PACKAGE_VERSION=`grep Version DESCRIPTION | awk '{print $2}'`
 
 # store the login credentials
 echo "[authentication]" > orig.synapseConfig
